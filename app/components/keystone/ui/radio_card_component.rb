5123c46dbb7794180e9f2cd33d4774d6cdba2741
tylercschneider <tylercschneider@gmail.com>
2026-06-07 19:48:59 -0400

    Expose radio card name and value

--- app/components/keystone/ui/radio_card_component.rb
+++ app/components/keystone/ui/radio_card_component.rb
@@ -3,7 +3,7 @@
 module Keystone
   module Ui
     class RadioCardComponent < ViewComponent::Base
-      attr_reader :label
+      attr_reader :name, :value, :label
 
       def initialize(name:, value:, label:, hint: nil, checked: false)
         @name = name
--- test/keystone/ui/radio_card_component_test.rb
+++ test/keystone/ui/radio_card_component_test.rb
@@ -8,4 +8,11 @@ class Keystone::Ui::RadioCardComponentTest < Minitest::Test
 
     assert_equal "Right now", component.label
   end
+
+  def test_exposes_name_and_value
+    component = Keystone::Ui::RadioCardComponent.new(name: "need", value: "now", label: "Right now")
+
+    assert_equal "need", component.name
+    assert_equal "now", component.value
+  end
 end
